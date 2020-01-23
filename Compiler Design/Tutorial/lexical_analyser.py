8ce514ad95ff399a394b6e2b37e275309bf3ff23
DivyPatel9881 <divy9881@gmail.com>
2020-01-23 15:17:41 +0530

    Bugs fixed.

--- Compiler Design/Tutorial/lexical_analyser.py
+++ Compiler Design/Tutorial/lexical_analyser.py
@@ -2,54 +2,57 @@ cpp_keywords = {"auto", "double", "int", "struct", "break", "else", "long", "swi
 
 operators = {"!=","!", "==", "=", "+", "-", "/", ",", "*", "|", "&", "||", "&&", "%", "^", "<=", ">=", "<", ">", "(", ")"}
 
-def Token_Analyzer(statement):
+def Token_Analyzer(statements):
+
     parameters = {"keywords":0, "operators":0, "identifiers": 0}
     tokens = []
-    start = 0
-    flag = None
 
-    if statement[0] in operators:
-        flag = "op"
-    else:
-        flag = "word"
-
-    for i in range(1,len(statement)):
-        if statement[i] in operators:
-            if flag=="op":
-                pass
+    for statement in statements.split(";"):
+        if statement != "":
+            start = 0
+            flag = None
+            if statement[0] in operators:
+                flag = "op"
             else:
-                token = statement[start:i]
-                l1 = token.split()
-                if len(l1) == 1:
-                    tokens.append(token)
-                    start = i
-                    flag = "op"
-                else:
-                    tokens.extend(l1)
-                    start = i
-                    flag = "op"
-        else:
-            if flag=="word":
-                pass
-            else:
-                tokens.append(statement[start:i])
-                start = i
                 flag = "word"
-    tokens.append(statement[start:])
+
+            for i in range(1,len(statement)):
+                if statement[i] in operators:
+                    if flag=="op":
+                        pass
+                    else:
+                        token = statement[start:i]
+                        l1 = token.split()
+                        if len(l1) == 1:
+                            tokens.append(token)
+                            start = i
+                            flag = "op"
+                        else:
+                            tokens.extend(l1)
+                            start = i
+                            flag = "op"
+                else:
+                    if flag=="word":
+                        pass
+                    else:
+                        tokens.append(statement[start:i])
+                        start = i
+                        flag = "word"
+            tokens.append(statement[start:])
 
     for token in tokens:
         if token.strip() in cpp_keywords:
             parameters["keywords"] += 1
         elif token.strip() in operators:
             parameters["operators"] += 1
-        elif token.isspace() == True:
-            pass
-        else:
+        elif token.strip().isidentifier()==True:
             parameters["identifiers"] += 1
+        else:
+            pass
 
     return parameters
 
 
 if __name__=="__main__":
-    statement = input()
-    print(Token_Analyzer(statement))
+    statements = input()
+    print(Token_Analyzer(statements))
